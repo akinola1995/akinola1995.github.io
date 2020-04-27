29d0a497b8f3fd7ba4b6ced80e245fafb8011ef0
akinola1995 <36579721+akinola1995@users.noreply.github.com>
2020-04-27 17:20:21 -0400

    Update new.js

--- new.js
+++ new.js
@@ -1,363 +1,364 @@
-/**
- * Pong game project
- * 
- */
+/ **
+ * Copyright (c) 2019
+ * 
+ * /
 
-// parameters for pong game
+// Size of the cubes for the game
 var UNITWIDTH = 90,
-    UNITHEIGHT = 45,
-    backgroundColor = 0xe0e0e0,
-    groundColor = 0xc21c23,
-    viewAngle = 40,
-    near = 1,
-    far = 2000,
-    PLAYERSPEED = 200, // How fast the player moves
-    direcaoBloco = 0,
-    collidableObjects = [],
-    PLAYERCOLLISIONDISTANCE = 20,
-    Largura_campo=500,
-    Comprimento_campo=1000,
-    score1=0,
-    score2=0,
-    cube1,
-    cube2;
+    UNITHEIGHT = 45,
+    backgroundColor = 0xe0e0e0,
+    groundColor = 0xc21c23,
+    viewAngle = 40,
+    near = 1,
+    far = 2000,
+    PLAYERSPEED = 200, // How fast the player moves
+    directionBlock = 0,
+    collidableObjects = [],
+    PLAYERCOLLISIONDISTANCE = 20,
+    Field_Width = 500,
+    Field_Length = 1000,
+    score1 = 0,
+    score2 = 0,
+    cube1,
+    cube2;
 
 // Flags to determine which direction the player is moving
 var moveLeft = false;
 var moveRight = false;
 
-scoreBoard = document.getElementById('scoreBoard');
+scoreBoard = document.getElementById ('scoreBoard');
 
 
-var camara, cena, renderer, fundo, direita, esquerda, player1, player2, cubo, relogio, delta, controlos;
-var velocidadeBloco = new THREE.Vector3(0,0,0);
+var camera, scene, renderer, background, right, left, player1, player2, cube, clock, delta, controls;
+var blockSpeed  = new THREE.Vector3 (0,0,0);
 
-// Setup do jogo
-init();
+// Game setup
+init ();
 animate();
 
 
-function init() {
-  relogio = new THREE.Clock();
-  // Criar a cena
-  cena = new THREE.Scene();
+function init () {
+  clock = new THREE.Clock ();
+  // Create the scene
+  scene = new THREE.Scene ();
 
-  // Definir render
-  renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor(backgroundColor);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  // Set render
+  renderer = new THREE.WebGLRenderer ();
+  renderer.setClearColor (backgroundColor);
+  renderer.setPixelRatio (window.devicePixelRatio);
+  renderer.setSize (window.innerWidth, window.innerHeight);
 
-  // Render para o container
-  var container = document.getElementById('container');
-  container.appendChild(renderer.domElement);
+  // Render to the container
+  var container = document.getElementById ('container');
+  container.appendChild (renderer.domElement);
 
-  // Definir a posiçao da camara
-  camara = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
-  camara.position.z = 800;
-  camara.position.y = 100;
+  // Define the position of the camera
+  camera = new THREE.PerspectiveCamera (40, window.innerWidth / window.innerHeight, 1, 10000);
+  camera.position.z = 800;
+  camera.position.y = 100;
 
-  //point camera on scene
-  camara.lookAt(velocidadeBloco);
+  // point camera on scene
+  camera.lookAt (blockSpeed);
 
-  cena.add(camara);
+  scene.add (camera);
 
-  controls = new THREE.PointerLockControls(camara);
-  cena.add(controls.getObject());
+  controls = new THREE.PointerLockControls (camera);
+  scene.add (controls.getObject());
 
-  // Add the walls(cubes) of the maze
-  criarFundo();
-  paredeTraseira();
-  criarParedes();
-  sliders();
-  Cubo();
-  updateScoreBoard();
- 
-  listenTeclasPressionadas();
+  // Add the walls (cubes) of the maze
+  createbackground();
+  rearwall();
+  createWalls();
+  sliders();
+  Cube();
+  updateScoreBoard();
+ 
+  listenPressedKeys();
 
-  // Add lights to the scene
-  luzes();
-  Bloqueio();
-  Bloqueio2();
+  // Add lights to the scene
+  lights();
+  Block();
+  Block2();
 
-  // Listen for if the window changes sizes and adjust
-  window.addEventListener('resize', onWindowResize, false);
+  // Listen for if the window changes sizes and adjust
+  window.addEventListener ('resize', onWindowResize, false);
 
 }
 
-//movimento do computador(jogador)
-function computador_jogador() {
-if(player1.position.x - 100 > cubo.position.x) {
-   player1.position.x -= Math.min(player1.position.x - cubo.position.x, 4);
- }else if(player1.position.x - 100 < cubo.position.x) {
-   player1.position.x += Math.min(cubo.position.x - player1.position.x, 4);
- }
- if(player1.position.x== player1.position.x){
-  player1.position.x += 5 ;
- }
+// computer movement (player)
+function computer_player() {
+if (player1.position.x - 100> cube.position.x) {
+   player1.position.x - = Math.min (player1.position.x - cubo.position.x, 4);
+ } else if (player1.position.x - 100 <cube.position.x) {
+   player1.position.x + = Math.min (cube.position.x - player1.position.x, 4);
+ }
+ if (player1.position.x == player1.position.x) {
+  player1.position.x + = 5;
+ }
 }
 
 // Add lights to the scene
-function luzes() {
-  var luz = new THREE.DirectionalLight(0xffffff, 1.2);
-  luz.position.set(1, 2, 1);
-  cena.add(luz);
+function lights() {
+  var light = new THREE.DirectionalLight (0xffffff, 1.2);
+  light.position.set (1, 2, 1);
+  scene.add(light);
 
 }
 
-//  Criar o fundo do jogo
-function criarFundo() {
-    var fundoGeometria = new THREE.BoxGeometry(Largura_campo, 1, Comprimento_campo),
-        fundoMaterial = new THREE.MeshPhongMaterial({color: groundColor}),
-        fundo = new THREE.Mesh(fundoGeometria, fundoMaterial);
-    fundo.position.set(0,0,0);
-    cena.add(fundo);
+// Create the game background
+function createbackground() {
+    var backgroundGeometry = new THREE.BoxGeometry (Width_field, 1, Length_field),
+        backgroundMaterial = new THREE.MeshPhongMaterial ({color: groundColor}),
+        background = new THREE.Mesh (backgroundGeometry, backgroundMaterial);
+    background.position.set (0,0,0);
+    scene.add(background);
 }
 
 
 
 
 
-function paredeTraseira() {
-    var fundoGeometria = new THREE.BoxGeometry(Largura_campo+40, 250, 1),
-        fundoMaterial = new THREE.MeshPhongMaterial({color: groundColor}),
-        fundo = new THREE.Mesh(fundoGeometria, fundoMaterial);
-    fundo.position.set(-5,140,-Comprimento_campo/2);
-    cena.add(fundo);
-   
+function backwall() {
+    var backgroundGeometry = new THREE.BoxGeometry (Width_field + 40, 250, 1),
+        backgroundMaterial = new THREE.MeshPhongMaterial ({color: groundColor}),
+        background = new THREE.Mesh (backgroundGeometry, backgroundMaterial);
+    background.position.set(-5,140, ​​-Length_field / 2);
+    scene.add(background);
+   
 }
 
-function criarParedes() {
-    var fundoGeometria = new THREE.BoxGeometry(20, 20, Comprimento_campo),
-        fundoMaterial = new THREE.MeshPhongMaterial({color: groundColor}),
-        esquerda = new THREE.Mesh(fundoGeometria, fundoMaterial),
-        direita = new THREE.Mesh(fundoGeometria, fundoMaterial);
-    esquerda.position.set(-Largura_campo/2-10,10,0);
-    direita.position.set(Largura_campo/2+10,10,0);
-    cena.add(direita);
-    cena.add(esquerda);
+function createWalls () {
+    var backgroundGeometry = new THREE.BoxGeometry (20, 20, Length_field),
+        backgroundMaterial = new THREE.MeshPhongMaterial ({color: groundColor}),
+        left = new THREE.Mesh (backgroundGeometry, backgroundMaterial),
+        right = new THREE.Mesh (backgroundGeometry, backgroundMaterial);
+    left.position.set(-Width_field / 2-10,10,0);
+    right.position.set(Field_ Width / 2 + 10,10,0);
+    scene.add(right);
+    scene.add(left);
 }
 
 function sliders() {
-    var playersGeometria = new THREE.BoxGeometry(100, 20, 10),
-    playersMaterial = new THREE.MeshPhongMaterial({color: 0x15ea9c});
-    player1 = new THREE.Mesh(playersGeometria, playersMaterial);
-    player2 = new THREE.Mesh(playersGeometria, new THREE.MeshPhongMaterial({color: 0xeaa615}));
-    player1.position.set(0,10,-Comprimento_campo/2 + 20);
-    player2.position.set(0,10,Comprimento_campo/2 - 20);
-    cena.add(player1);
-    cena.add(player2);
-    collidableObjects.push(player1);
-    collidableObjects.push(player2);
+    var playersGeometry = new THREE.BoxGeometry (100, 20, 10),
+    playersMaterial = new THREE.MeshPhongMaterial ({color: 0x15ea9c});
+    player1 = new THREE.Mesh (playersGeometry, playersMaterial);
+    player2 = new THREE.Mesh (playersGeometry, new THREE.MeshPhongMaterial ({color: 0xeaa615}));
+    player1.position.set(0.10, -Length_field / 2 + 20);
+    player2.position.set(0.10, Length_field / 2 - 20);
+    scene.add(player1);
+    scene.add(player2);
+    collidableObjects.push(player1);
+    collidableObjects.push(player2);
 }
 
-function Cubo() {
-  var cuboGeometria = new THREE.BoxGeometry(10,10,10),
-      cuboMaterial = new THREE.MeshPhongMaterial({color: backgroundColor});
-  cubo = new THREE.Mesh(cuboGeometria,cuboMaterial);
-  cubo.position.set(0,5,0);
-  cena.add(cubo);
-  collidableObjects.push(cubo);
+function Cube() {
+  var cubeGeometry = new THREE.BoxGeometry (10,10,10),
+      cubeMaterial = new THREE.MeshPhongMaterial ({color: backgroundColor});
+  cube = new THREE.Mesh (cubeGeometry, cubeMaterial);
+  cube.position.set (0,5,0);
+  scene.add (cube);
+  collidableObjects.push(cube);
 }
 
 
 
-function Bloqueio(){
+function Block() {
 
-  var cubeGeometry = new THREE.BoxGeometry(30, 30, 30);
-  var cubeMaterial =  new THREE.MeshPhongMaterial({color: groundColor});
-  var cu = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cu.position.set(RandomXCube(), 10, RandomYCube());
-  cube1=cu;
-  cena.add(cube1);
- 
+  var cubeGeometry = new THREE.BoxGeometry (30, 30, 30);
+  var cubeMaterial = new THREE.MeshPhongMaterial ({color: groundColor});
+  var cu = new THREE.Mesh (cubeGeometry, cubeMaterial);
+  cu.position.set (RandomXCube(), 10, RandomYCube());
+  cube1 = cu;
+  scene.add(cube1);
+ 
 }
 
-function Bloqueio2(){
 
-  var cubeGeometry = new THREE.BoxGeometry(30, 30, 30);
-  var cubeMaterial =  new THREE.MeshPhongMaterial({color: groundColor});
-  var cu = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cu.position.set(RandomXCube(), 10, RandomYCube());
-  cube2=cu;
-  cena.add(cube2);
- 
+function Block2() {
+
+  var cubeGeometry = new THREE.BoxGeometry (30, 30, 30);
+  var cubeMaterial = new THREE.MeshPhongMaterial ({color: groundColor});
+  var cu = new THREE.Mesh (cubeGeometry, cubeMaterial);
+  cu.position.set (RandomXCube(), 10, RandomYCube());
+  cube2 = cu;
+  scene.add (cube2);
+ 
 }
 
 function RandomXCube() {
-  return Math.random() * ((Largura_campo/2-50) - (-Largura_campo/2+50)) + (-Largura_campo/2+50) ;
+  return Math.random() * ((Field_Width /2-50) - (-Field_Width /2 + 50)) + (-Field_Width /2 + 50);
 }
 
-function RandomYCube() {
-  return Math.random() * ((Comprimento_campo/2-200) - (-Comprimento_campo/2+200)) + (-Comprimento_campo/2+200);
-  
+function RandomYCube () {
+  return Math.random () * ((Length_field /2-200) - (-Length_field /2 + 200)) + (-Length_field /2 + 200);
+  
 }
 
 
-function listenTeclasPressionadas() {
-  // Tecla pressionada
+function listenPressedKeys() {
+  // Key pressed
 
-  var teclaPressionada = function(event) {
-    switch (event.which) {
-      case 37: Esquerda();
-        break;
-      case 39: Direita();
-        break;
-      case 32:
-        reset()
-        break;
-    }
-  };
+  varPressedKeys = function(event) {
+    switch (event.which) {
+      case 37: Left();
+        break;
+      case 39: Right();
+        break;
+      case 32:
+        reset()
+        break;
+    }
+  };
 
-  // Tecla solta
-  var teclaSolta = function(event) {
-  switch (event.which) {
-    case 37:  
-    player2.position.x -=20;
-      break;
-   case 39: 
-   player2.position.x += 20;
-      break;
-  }
+  // Back key
+  var Backkey = function (event) {
+  switch (event.which) {
+    case 37:
+    player2.position.x - = 20;
+      break;
+   case 39:
+   player2.position.x + = 20;
+      break;
+  }
 };
 
 // Add event listeners for when movement keys are pressed and released
-document.addEventListener('keydown', teclaSolta, false);
-document.addEventListener('keyup', teclaPressionada, false);
+document.addEventListener ('keydown', backkey, false);
+document.addEventListener ('keyup', Pressedkey, false);
 }
 
-function Direita(){
-    player2.position.x += 10;
+function Right() {
+    player2.position.x + = 10;
 }
-function Esquerda(){
-    player2.position.x -= 10;
-}
-
-function controlarCubo() {
-
-  
-  if(!cubo.$velocity){
-    comecarMovimentoCubo();
-  }
-  atualizarPosicaoCubo();
-  if(cubo.position.z == player2.position.z && (cubo.position.x < player2.position.x+ 50 && cubo.position.x > player2.position.x - 50 ) ){
-    cubo.$velocity.x = (cubo.position.x - player2.position.x) / 3;
-    cubo.$velocity.z *= -1;
-  }
-  if(cubo.position.z == player1.position.z && (cubo.position.x < player1.position.x+ 50 && cubo.position.x > player1.position.x - 50 ) ){
-    cubo.$velocity.x = (cubo.position.x - player1.position.x) / 3;
-    cubo.$velocity.z *= -1;
-  }
-  
-  if(cubo.position.z > player2.position.z+ 1000 ){
-    score1= score1+1;
-    updateScoreBoard();
-    reset();
-  }
-  if(cubo.position.z < player1.position.z -1000){
-    score2= score2+1;
-    updateScoreBoard();
-    reset();
-  }
-
- 
-
-  if(isSideCollision()) {
-    cubo.$velocity.x *= -1;   console.log("reset");
-
-  }
-
-
- if(cubo.position.x < cube1.position.x+15 && cubo.position.x > cube1.position.x-15 && cubo.position.z < cube1.position.z+15 && cubo.position.z > cube1.position.z-15){
-    cubo.$velocity.x *= -1;
-    cubo.$velocity.z *= -1;
-
-  }
-
- if(cubo.position.x < cube2.position.x+15 && cubo.position.x > cube2.position.x-15 && cubo.position.z < cube2.position.z+15 && cubo.position.z > cube2.position.z-15){
-  cubo.$velocity.x *= -1;
-  cubo.$velocity.z *= -1;
-
-}
+function Left() {
+    player2.position.x - = 10;
 }
 
+function controlCube() {
 
-// Ajuste do tamanho do ecra
+  
+  if(! cube. $ velocity) {
+    startCubeMovement();
+  }
+  updateCubePosition();
+  if(cube.position.z == player2.position.z && (cube.position.x <player2.position.x + 50 && cube.position.x> player2.position.x - 50)) {
+    cube. $velocity.x = (cube.position.x - player2.position.x) /3;
+    cube. $velocity.z * = -1;
+  }
+  if(cube.position.z == player1.position.z && (cube.position.x <player1.position.x + 50 && cube.position.x> player1.position.x - 50)) {
+    cube. $velocity.x = (cube.position.x - player1.position.x) /3;
+    cube. $velocity.z * = -1;
+  }
+  
+  if(cube.position.z> player2.position.z + 1000) {
+    score1 = score1 + 1;
+    updateScoreBoard();
+    reset();
+  }
+  if(cube.position.z <player1.position.z -1000) {
+    score2 = score2 + 1;
+    updateScoreBoard();
+    reset();
+  }
+
+ 
+
+  if (isSideCollision()) {
+    cube. $velocity.x * = -1; console.log ("reset");
+
+  }
+
+
+ if (cube.position.x <cube1.position.x + 15 && cubo.position.x> cube1.position.x-15 && cubo.position.z <cube1.position.z + 15 && cubo.position.z> cube1.position.z-15) {
+    cube. $velocity.x * = -1;
+    cube. $velocity.z * = -1;
+
+  }
+
+ if (cubo.position.x <cube2.position.x + 15 && cubo.position.x> cube2.position.x-15 && cubo.position.z <cube2.position.z + 15 && cubo.position.z> cube2.position.z-15) {
+  cube. $velocity.x * = -1;
+  cube. $velocity.z * = -1;
+
+}
+}
+
+
+// Adjusting the screen size
 function onWindowResize() {
-  camara.aspect = window.innerWidth / window.innerHeight;
-  camara.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix ();
+  renderer.setSize (window.innerWidth, window.innerHeight);
 }
 
-// Animar o jogo
-function animate() {
-  render();
-  // Keep updating the renderer
-  requestAnimationFrame(animate);
-  controlarCubo();
-  computador_jogador();
+// Animate the game
+function animate () {
+  render ();
+  // Keep updating the renderer
+  requestAnimationFrame (animate);
+  controlCube ();
+  computer_player ();
 
 }
 
-// Parar cubo
-function pararCubo() {
-  cubo.$stoped = true;
+// Stop cube
+function stopCube () {
+  cube. $stoped = true;
 }
 
-// Reset do Jogo
-function reset() {
-  console.log("reset");
-  cubo.position.set(0,10,0);
-  cubo.$velocity= null;
-  player1.position.x=0;
-  player2.position.x=0;
+// Game Reset
+function reset () {
+  console.log ("reset");
+  cubo.position.set (0,10,0);
+  cube. $velocity = null;
+  player1.position.x = 0;
+  player2.position.x = 0;
 }
 
-// Começao movimento do cubo 
-function comecarMovimentoCubo() {
-  var direcao = Math.random() > 0.5 ? -1 : 1;
-  cubo.$velocity = {
-    x: 0,
-    z: direcao * 20
-  };
+// Start cube movement
+function getMotionCube () {
+  direction var = Math.random ()> 0.5? -1: 1;
+  cube. $ velocity = {
+    x: 0,
+    z: direction * 20
+  };
 }
 
-//Atualizar a posição do cubo
-function atualizarPosicaoCubo() {
-  var cuboPos = cubo.position;
-  cuboPos.x += cubo.$velocity.x;
-  cuboPos.z += cubo.$velocity.z;
+// Update the cube position
+function updateCubePosition() {
+  var cuboPos = cubo.position;
+  cubePos.x + = cube. $velocity.x;
+  cuboPos.z + = cube. $velocity.z;
 
 }
 
 
 
 // Render the scene
-function render() {
-  //requestAnimationFrame(render);
-  
-  renderer.render(cena, camara);
+function render () {
+  // requestAnimationFrame (render);
+  
+  renderer.render (scene, camera);
 }
 
 // Converts degrees to radians
 function degreesToRadians(degrees) {
-  return degrees * Math.PI / 180;
+  return degrees * Math.PI / 180;
 }
 
 // Converts radians to degrees
 function radiansToDegrees(radians) {
-  return radians * 180 / Math.PI;
+  return radians * 180 / Math.PI;
 }
 
 
 function isSideCollision() {
-  var cuboX = cubo.position.x,
-      metade = Largura_campo / 2;
-  return cuboX - 5 < -metade ||cuboX + 5 > metade;
+  var cubeX = cube.position.x,
+      half = Field_Width / 2;
+  return cubeX - 5 <-half || cubeX + 5> half;
 }
 
 
-  
+  
 function updateScoreBoard() {
-  scoreBoard.innerHTML = 'PC-' + score1 + '  vs  ' + score2+ '-Jogador' ;
+  scoreBoard.innerHTML = 'PC-' + score1 + 'vs' + score2 + '-Player';
 }
